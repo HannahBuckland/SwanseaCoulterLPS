00a4a5a8b608d0a6dc96771a8befeb9dd5f66a43
Hannah Buckland <hannahbuckland@Hannahs-MacBook-Pro.local>
2022-01-21 14:56:11 +0000

    updating plot code

--- code/file_process_function.R
+++ code/file_process_function.R
@@ -10,5 +10,27 @@ LPS_file <- "data/20220120/CITEST_CI_Rom_12.txt"
 
 LPS_data <- read.table(file=LPS_file,
                        sep="\t",
-                       skip=95,
+                       skip=66,
                        fill=TRUE)
+
+LPS_data <- LPS_data[1:28,] %>%
+  mutate_if(is.character,as.numeric)
+
+colnames(LPS_data) <- c("lower_um",
+                        "cumul.vol_lessthan",
+                        "cumul.vol_greatthan",
+                        "vol_perc")
+
+LPS_phi <- LPS_data %>%
+  mutate(lower_phi = round(-log((lower_um/1000),2),1))
+    
+
+ggplot(LPS_data) +
+  geom_path(aes(x=lower_um,
+                y=cumul.vol_lessthan)) +
+  scale_x_log10()
+
+ggplot(LPS_phi) +
+  geom_col(aes(x=lower_phi,
+                y=vol_perc)) +
+  scale_x_reverse()
