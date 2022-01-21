c46d92669229c3ee26a97d559cd8957eed6976f3
Hannah Buckland <hannahbuckland@Hannahs-MacBook-Pro.local>
2022-01-21 14:11:32 +0000

    python script to change weird file names and extensions

---
+++ code/file_ext_change.py
@@ -0,0 +1,15 @@
+# Script to change file extension of LPS data files
+# Hannah Buckland
+# 21/01/2022
+
+
+import os
+import sys
+folder = 'data/20220120/'
+
+for filename in os.listdir(folder):
+  infilename = os.path.join(folder,filename)
+  base_file, ext = os.path.splitext(filename)
+  if ext == ".xls":
+    output = os.rename(infilename, infilename.replace('.$ls.xls', '.txt'))
+  
