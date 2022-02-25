7922be93eead6aa9b8ce3a901f672d8d04739c82
Hannah Buckland <h.m.buckland@swansea.ac.uk>
2022-02-25 11:14:59 +0000

    updated python script to creat archive of original file extensions

--- code/file_ext_change.py
+++ code/file_ext_change.py
@@ -1,15 +1,23 @@
 # Script to change file extension of LPS data files
 # Hannah Buckland
 # 21/01/2022
+# modified 25/02/2022
 
 
 import os
 import sys
-folder = '../data/20220119/'
+import shutil
 
-for filename in os.listdir(folder):
-  infilename = os.path.join(folder,filename)
+src_dir = os.getcwd() # gets current working directory
+
+dest_dir = "Coulter_files" # set name of new directory
+
+shutil.copytree(src_dir,dest_dir)
+
+for filename in os.listdir(src_dir):
+  infilename = os.path.join(src_dir,filename)
   base_file, ext = os.path.splitext(filename)
   if ext == ".xls":
     output = os.rename(infilename, infilename.replace('.$ls.xls', '.txt'))
   
+
